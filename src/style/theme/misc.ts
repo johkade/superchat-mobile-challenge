35c8c89c2d3c12782c9d1534455aeb23f48d0f56
johkade <johkade.dev@gmail.com>
2022-06-04 11:13:22 +0200

    -changed: styling constants

--- src/style/theme/misc.ts
+++ src/style/theme/misc.ts
@@ -1,25 +1,22 @@
-export const ACTIVE_OPACITY = .5;
+export const ACTIVE_OPACITY = 0.5;
 
 export const SPACE = {
-    // special
-    topPadding: 12,
-    sidePadding: 24,
-    searchBarHeight: 56,
-
-    xxl32: 32,
-    xl24: 24,
-    l16: 16,
-    m8: 8,
-    s12: 12,
-    xs2: 2,
-    xxs1: 1,
-}
+  xxl32: 32,
+  xl24: 24,
+  l16: 16,
+  m12: 12,
+  s8: 8,
+  xs4: 4,
+  xxs2: 2,
+};
 export const BORDER_RADIUS = {
-    full: 1000,
-    m: 12,
-}
+  full: 1000,
+  m: 12,
+};
 
 export const ICON_SIZE = {
-    m16: 16,
-    l24: 24,
-}
+  l24: 24,
+  m16: 16,
+  s12: 12,
+  xs8: 8,
+};
